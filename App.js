1892c1ec4c2921326b059028e65b13100a586875
Kevin Putera <kevintiosanna@gmail.com>
2020-01-18 22:22:57 +0800

    Reorganize directories

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
-import { createAppContainer } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
+import MapScreen from './src/screens/MapScreen';
+
+const AppStack = createStackNavigator(
+  {
+    Map: MapScreen,
+  },
+  {
+    headerMode: 'none',
+  }
+);
+const AuthStack = createStackNavigator(
+  {
+    Login: LoginScreen,
+    Signup: SignupScreen,
+  },
+  {
+    headerMode: 'none',
+  }
+);
 
 export default createAppContainer(
-  createStackNavigator(
+  createSwitchNavigator(
     {
-      Login: LoginScreen,
-      Signup: SignupScreen,
+      Auth: AuthStack,
+      App: AppStack,
     },
     {
-      headerMode: 'none',
-      initialRouteName: 'Login',
+      initialRouteName: 'App',
     }
   )
 );
